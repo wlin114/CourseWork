1272dd653fba2ef0039916f3cc0a5f1b095bcaae
weiwei lin <wlin114@outlook.com>
2014-11-09 16:26:23 -0500

    4:26

--- Python_Max_wiz.py
+++ Python_Max_wiz.py
@@ -1,13 +1,8 @@
-a = []
-n = len(a)
-best = [0] * n 
-best[1] = max(0, a[1])
-back = {}
-
 def bottom_up():
 	for i in range(1, n):
 		best[i] = max(best[i-1], best[i-2]+a[i])
 		back[i] = best[i] == best[i-1]
+	return best[-1]
 
 def top_down(i):
 	if i in best:
@@ -25,9 +20,27 @@ def solution(i):
 		return solution(i-1)
 	return solution(i-2) + [a[i]]
 
-bottom_up()
-print best[-1]
-print solution(n-1)
-best = {0:0, 1: max(0,a[1])}
-print top_down(n-1)
-print solution(n-1)
+def max_wiz(list):
+	if list == []:
+		print (0, [])
+	else:
+		global a, n, best, back
+		a = [0] + list
+		n = len(a)
+		best = [0] * n
+		best[1] = max(0, a[1])
+		back = {}
+
+		result = bottom_up()
+		path = solution(n-1)
+		print "Bottom up:"
+		print (result, path)
+
+		best = {0:0, 1: max(0,a[1])}
+
+		result = top_down(n-1)
+		path = solution(n-1)
+		print "Top down:"
+		print (result, path)
+
+max_wiz([])
