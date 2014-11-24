112d724d800426116da8042fcc37782c573c2d2c
weiwei lin <wlin114@outlook.com>
2014-11-24 14:37:59 -0500

    2:27

--- Python_bfs_directed.py
+++ Python_bfs_directed.py
@@ -1,6 +1,6 @@
 color = {"A":0,"B":0,"C":0,"D":0,"E":0}
 graph = {"A": ["B"], "B": ["C", "D", "E"], "C": ["E"], "D": ["A", "E"]}
-q = ["A"]
+q = [("","A")]
 visit = ""
 back = []
 cross = []
@@ -9,18 +9,19 @@ def bfs():
 	while q!=[]:
 		global visit
 		head = q.pop(0)
-		if head not in graph and color[head]!=2:
-			visit = visit + " " + head
-			color[head] = 2
-		elif color[head] == 0:
-			visit = visit + " " + head
-			color[head] = 1
-			q.extend(graph[head])
-		elif color[head] == 1:
+		if head[1] not in graph and color[head[1]]!=2:
+			visit = visit + head[1] + " "
+			color[head[1]] = 2
+		elif color[head[1]] == 0:
+			visit = visit + head[1] + " "
+			color[head[1]] = 1
+			for i in graph[head[1]]:
+				q.append((head[1], i))
+		elif color[head[1]] == 1:
 			back.append(head)
-		elif color[head] == 2:
-			cross.append(head)
+		elif color[head[1]] == 2:
+			cross.append(head[1])
 
 
 bfs()
-print visit,  back, 
+print visit,  back
